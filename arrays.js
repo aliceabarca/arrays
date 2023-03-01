b065ab640008942691b1a7743fc88f52cfce28ff
Alice Abarca <aliceabarca@otmail.com>
2023-03-01 14:11:43 -0700

    Add array methods and explanation

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
 var friends = [ "Janice", "Justin", "Ian", "Alli"];
+console.log(friends.pop());
+// the pop() method returns the last element of the array.
+
 var friendsAge = [27, 24, 31, 26];
+console.log(friendsAge.slice(2));
+// the slice() method returns only a portion.
+
 var friendsInState = [false, true, true, false];
+console.log(friendsInState.push(false));
+// the push() method adds more elements.
